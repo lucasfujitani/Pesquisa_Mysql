2763fb1e00bc1f55632c80e731b9b9c72c973cb6
Lucas Fujitani <castrofujitani@gmail.com>
2023-09-26 22:56:11 -0300

    Files

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.sql.*;
+import com.mysql.cj.jdbc.Driver;
+
+public class Main {
+    public static void main(String[] args) {
+buscarUsuarios();
+
+        }
+        public static void buscarUsuarios(){
+
+        try{
+            Driver driver = new Driver();
+            DriverManager.registerDriver(driver);
+
+            Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/sakila", "root", "password");
+            PreparedStatement stmt = c.prepareStatement("SELECT * FROM customer");
+            stmt.executeQuery();
+
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                rs.next();
+                String nome = rs.getString("first_name");
+                String email = rs.getString("email");
+
+                System.out.println("Nome: " + nome);
+                System.out.println("Email: " + email);
+
+            }
+c.close();
+
+
+        }catch (SQLException  e){
+            e.printStackTrace();
+        }
+        }
+    }
